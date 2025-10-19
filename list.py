04a5a66f0d8d13e78188b2b1232728706c726069
Richard Addo <ramankwah03@gmail.com>
2025-10-19 09:22:17 +0200

    initial commit

---
+++ list.py
@@ -0,0 +1,9 @@
+import random
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+
+# for index, item in enumerate(supplies):
+#     print(index)
+
+r= random.choice(supplies)
+k=random.shuffle(supplies)
+print(k)
