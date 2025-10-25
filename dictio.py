9bfb947f2794533016317ed63d9030abbfdeb3aa
Richard Addo <ramankwah03@gmail.com>
2025-10-25 15:30:46 +0200

    dic added

---
+++ dictio.py
@@ -0,0 +1,10 @@
+spam = {'color': 'red', 'age': 42}
+for k,v in spam.items():
+    if 
+    print(k)
+
+# print(spam.get('egg', "NOTHING"))
+
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
--- list.py
+++ list.py
@@ -1,9 +1,16 @@
 import random
 supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+spam = ['Ants', 'Cats', 'Dogs', 'Badgers', 'Elephants']
 
 # for index, item in enumerate(supplies):
 #     print(index)
 
 r= random.choice(supplies)
 k=random.shuffle(supplies)
-print(k)
+spam.sort()
+print(spam)
+
+
+s = ['a', 'z', 'A', 'Z']
+s.sort(key=str.lower)
+print(s)
